ce2794370c76677ac249133b29c725fc028662eb
Sally Resch <sallyresch27@gmail.com>
2023-01-09 12:48:55 +0100

    Added variables to User-class

--- src/main/java/com/sillysally/kyst/user/User.java
+++ src/main/java/com/sillysally/kyst/user/User.java
@@ -1,4 +1,12 @@
 package com.sillysally.kyst.user;
 
 public class User {
+
+
+    private Long id;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private int phoneNumber;
+    private String password;
 }
