0ebd5464dc56dceb6febf8783a3d6c53b04a6d9f
Sally Resch <sallyresch27@gmail.com>
2023-01-09 12:23:38 +0100

    Initial commit

---
+++ src/main/java/com/sillysally/kyst/KystApplication.java
@@ -0,0 +1,13 @@
+package com.sillysally.kyst;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KystApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KystApplication.class, args);
+    }
+
+}
