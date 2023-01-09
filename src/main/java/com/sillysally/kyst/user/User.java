fee5c0a29f048520cba9c5ffc0691cb9a1c70c78
Sally Resch <sallyresch27@gmail.com>
2023-01-09 13:05:04 +0100

    Commented back the jpa dependencie and made UserRepository.java. Also made consturctor and toString in User.java

--- src/main/java/com/sillysally/kyst/user/User.java
+++ src/main/java/com/sillysally/kyst/user/User.java
@@ -1,12 +1,49 @@
 package com.sillysally.kyst.user;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name="Users")
 public class User {
 
-
+    @Id
+    @GeneratedValue
     private Long id;
+
     private String firstName;
     private String lastName;
     private String email;
     private int phoneNumber;
     private String password;
+
+    public User(String firstName,
+                String lastName,
+                String email,
+                int phoneNumber,
+                String password) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.phoneNumber = phoneNumber;
+        this.password = password;
+    }
+
+    public User() {
+
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", email='" + email + '\'' +
+                ", phoneNumber=" + phoneNumber +
+                ", password='" + password + '\'' +
+                '}';
+    }
 }
