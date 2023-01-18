d70dda3bba5da5d3abdbbaa8131f3bbae0cb9cfe
Sally Resch <sallyresch27@gmail.com>
2023-01-18 12:21:23 +0100

    checked that posting and geting users worked with postman

--- src/main/java/com/sillysally/kyst/user/UserRepository.java
+++ src/main/java/com/sillysally/kyst/user/UserRepository.java
@@ -6,5 +6,5 @@ import java.util.Objects;
 import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Long> {
-Optional<Object> findCustomerById(Long id);
+Optional<Objects> findCustomerById(Long id);
 }
